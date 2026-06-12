71272a1178e80aef5fcc4105ff0e584f84773a4b
isacarrd <rcpessoaluso@gmail.com>
2026-06-11 22:32:08 -0300

    EX: operador spread

---
+++ spread.js
@@ -0,0 +1,29 @@
+// Exercício 1.1: Mesclando Turmas
+// const turmaManha = ["Ana", "João"]
+// const turmaTarde = ["Maria", "Pedro"]
+// const turmaEvento = [...turmaManha, ...turmaTarde]
+// console.log(turmaEvento)
+
+// Exercício 1.2: Atualizando o Perfil do Usuário
+// const usuario = {
+//   nome: "Carlos",
+//   idade: 28,
+//   email: "c@c.com"
+// }
+// const novoUsuario = { ...usuario, idade: 30, status: "ativo" }
+// console.log(novoUsuario)
+
+// Exercício 1.3: Quebrando uma String
+// const palavra = "React"
+// const palavraArray = [...palavra]
+// console.log(palavraArray)
+
+// Exercício 1.4: Encontrando o Maior Valor
+// const temperaturas = [22, 28, 35, 19, 31]
+// const maxTemp = Math.max(...temperaturas)
+// console.log(maxTemp)
+
+// Exercício 1.5: Cópia Segura (Evitando Mutação)
+// const carrinho = ["Maçã", "Banana"]
+// const copCarrinho = [...carrinho, "Uva"]
+// console.log(`Cópia: ${copCarrinho}, Original: ${carrinho}`)
