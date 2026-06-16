f378e5273a81eee9739f4da094b94139a7f94ded
isacarrd <rcpessoaluso@gmail.com>
2026-06-15 22:23:37 -0300

    update: exercício de funções com retorno

---
+++ functionParam.js
@@ -0,0 +1,33 @@
+// Exercício 2.1: Conversão Cambial Fixa
+/* const taxaMerc = 5.50
+function converterDolarParaReal(valorUs) {
+  let valorReais = valorUs * taxaMerc
+  console.log(valorReais)
+}*/
+
+//Exercício 2.2: Verificação de Campo Vazio
+/* function isCampoVazio(valCampo) {
+  return " " ? true : false
+}
+console.log(isCampoVazio("")) */
+
+// Exercício 2.3: Calculadora de Cupom de Desconto
+/* function aplicarCupom15(valBruto) {
+  const desconto = 15
+  let valDescontado = valBruto * desconto / 100
+  let valorLiquido = valBruto - valDescontado
+  return valorLiquido
+}
+console.log(aplicarCupom15(200)) */
+
+//Exercício 2.4: Validador de Maioridade
+/* function podeAcessarConteudo(idade) {
+  return idade >= 18 ? true : false
+}
+console.log(podeAcessarConteudo(17)) */
+
+//Exercício 2.5: Formatador de Nomes de Arquivos
+/* function gerarNomeArquivoTemp(num) {
+  return `temp_id_${num}.json`
+}
+console.log(gerarNomeArquivoTemp(104)) */
