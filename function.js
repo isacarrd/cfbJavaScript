e5cd317c0221343a7cbc8bf0b3561679c22615d9
isacarrd <rcpessoaluso@gmail.com>
2026-06-15 22:00:11 -0300

    update: exercício de function normais de console

---
+++ function.js
@@ -0,0 +1,29 @@
+// Exercício 1.1: Boas - vindas ao Sistema
+/* function inicializarApp() {
+  console.log("Sistema iniciado com sucesso. Aguardando Autenticação...")
+}
+inicializarApp() */
+
+// Exercício 1.2: Alerta de Falha Crítica
+/* function emitirAlertaConexao() {
+  console.log("ERRO CRÍTICO: Falha na conexão com o servidor de dados!")
+}
+emitirAlertaConexao() */
+
+// Exercício 1.3: Divisor de Logs
+/* function imprimirDivisor() {
+  console.log("--------------------")
+}
+imprimirDivisor() */
+
+// Exercício 1.4: Notificação de Sessão Expirada
+/* function notificarTimeout() {
+  console.log("Sessão expirada por inatividade. Por favor, refaça o login.")
+}
+notificarTimeout() */
+
+// Exercício 1.5: Logs de Persistência
+/* function confirmarSalvamentoLocal() {
+  console.log("Modificações gravadas com sucesso no armazenamento local.")
+}
+confirmarSalvamentoLocal() */
