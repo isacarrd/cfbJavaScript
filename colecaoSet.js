d6f7579402c7e37b1b7cfb2a2c9cd29a3283c461
isacarrd <rcpessoaluso@gmail.com>
2026-08-05 20:26:14 -0300

    add: só comentários de correção do GEMINI

--- colecaoSet.js
+++ colecaoSet.js
@@ -1,5 +1,6 @@
 /* // Exercício 2.1: Sistema de Tags Limpo
 const tagsPost = ["javascript", "html", "css", "javascript", "react", "html"]
+// Correção do GEMINI -> não está retornando um array definitivo, e só seria Array se [...new Set(tagsPost)]
 const tag = new Set([...tagsPost])
 console.log(tag) */
 
@@ -12,6 +13,8 @@ const userA = ["Bia", "Carlos", "Daniel", "Ester"]
 const userB = ["Carlos", "Ester", "Felipe", "Bia"]
 
 const userASet = new Set(userA)
+
+// Correção do GEMINI -> não precisa do operador ternário, pois o .filter() espera um true ou false enquanto o .has() devolve um booleano, sendo assim: const usersFiltrados = userB.filter((user) => userASet.has(user))
 const usersFiltrados = userB.filter((user) => {
   return userASet.has(user) ? user : ""
 })
@@ -31,5 +34,7 @@ const IPSabado = ["IP-1", "IP-2", "IP-3"]
 const IPDomingo = ["IP-2", "IP-3", "IP-4", "IP-5"]
 
 const IPSFDS = IPSabado.concat(IPDomingo)
+
+// Correção do GEMINI -> não está retornando um array definitivo, e só seria Array se [...new Set(IPSFDS)]
 const ipsTotais = new Set(IPSFDS)
 console.log(ipsTotais) */
